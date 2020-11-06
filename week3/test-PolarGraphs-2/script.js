372042420cf5511410691567b0baf6b5b0330506
Nan Zhao <nanzhao@nyu.edu>
2020-11-06 23:11:16 +0000

    positionX and position Y are from another wave array

--- week3/test-PolarGraphs-2/script.js
+++ week3/test-PolarGraphs-2/script.js
@@ -1,3 +1,4 @@
+//version log: positionX and positionY is another wave array
 const playButton = document.getElementById('playButton');
 var changeThis = 1;
 var maximJs = maximilian();
@@ -7,7 +8,11 @@ var maxiAudio = new maximJs.maxiAudio();
 var osc = new maximJs.maxiOsc();
 var osc2 = new maximJs.maxiOsc();
 var osc3 = new maximJs.maxiOsc();
+var osc4 = new maximJs.maxiOsc();
+var osc5 = new maximJs.maxiOsc();
+var osc6 = new maximJs.maxiOsc();
 var drawOutput = new Array(1024);
+var drawCentre = new Array(1024);
 var counter = 0;
 
 //var canvas = document.getElementById("canvas");
@@ -29,10 +34,13 @@ let playAudio = () => {
     maxiAudio.play = function(){
         //var wave = (osc.sawn(bufferFreq) - osc2.sawn(bufferFreq*1.001));
         var wave = osc.sinewave(bufferFreq+osc2.sinewave(bufferFreq*changeThis)*osc3.sinewave(0.01)*1000);
-        //var wave = osc.sinewave(bufferFreq*osc2.sinewave(bufferFreq));
+        //var wave2 = osc4.sinewave(bufferFreq*osc5.sinewave(bufferFreq*1)*osc6.sinewave(0.01)*100);
+        // var wave2 = osc3.sinewave(bufferFreq*osc4.sinewave(bufferFreq));
+        var wave2 = (osc.sawn(bufferFreq) - osc3.sawn(bufferFreq*10.01));
         counter++;
 
         drawOutput[counter % 1024] = wave;
+        drawCentre[counter % 1024] = wave2;
         return wave * 0.0;
     }
 
@@ -47,15 +55,15 @@ function draw() {
 
     context.clearRect(0, 0, width, height);
     //var spacing = ((Math.PI * 2) / 1024);
-    var spacing = ((Math.PI * 2) / 1024 * 4);
+    var spacing = ((Math.PI * 2) / 1024 );
     var size = 200;
 
     for (var i = 0; i < 1024; i++) {
-        positionX = positionBaseX + Math.cos(i*spacing)*size/2;
-        positionY = positionBaseY + Math.sin(i*spacing)*size/2;
+        positionX = positionBaseX + Math.cos(i*spacing)*size*2 *drawCentre[i];
+        positionY = positionBaseY + Math.sin(i*spacing)*size*2 * drawCentre[i];
         context.beginPath();
         //there is a more efficient way to do the code.
-        context.moveTo(positionX + (Math.cos(i * spacing) * size * drawOutput[i]),positionY + (Math.sin(i * spacing) * size * drawOutput[i]));
+        context.moveTo(positionBaseX + (Math.cos(i * spacing) * size * drawOutput[i]),positionBaseY + (Math.sin(i * spacing) * size * drawOutput[i]));
         //context.lineTo(positionX+ 100 + Math.sin(i * spacing)*100,positionY -100 + Math.cos(i * spacing)*100);
         context.lineTo(positionX,positionY );
         //context.lineTo(position + (Math.cos(i * spacing) ),(height / 2) + (Math.sin(i * spacing)));
