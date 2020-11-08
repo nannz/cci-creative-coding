53204a6e4e2b0a63d8636b198dc633cd5aafa598
Nan Zhao <nanzhao@nyu.edu>
2020-11-08 16:01:46 +0000

    add test-algorithm1 for testing Parametric lines

--- week3/test-algorithm1/script.js
+++ week3/test-algorithm1/script.js
@@ -9,46 +9,43 @@ var TWO_PI = Math.PI * 2;
 var canvas = document.querySelector("canvas");
 
 // This sets the width and height to the document window
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 500;//window.innerWidth;
+canvas.height = 500;//window.innerHeight;
 // Be aware that when you resize the window, you will need to call (do) this again
 
 // This creates a 2d drawing 'context' in your canvas
 // All your drawing will be done in this canvas
 var context = canvas.getContext("2d");
-
 //This tells the browser to get the mouse information from the function we've called getMouse
 canvas.addEventListener('mousemove', getMouse, false);
-
 //here's our function 'getMouse'.
 function getMouse(mousePosition) {
     mouseX = mousePosition.layerX;
     mouseY = mousePosition.layerY;
 }
 
+
+
 // This function translates the canvas so that we're looking at it from a different position, meaning that 0,0 is somewhere else
 function draw() {
-
-    var segments = 1000;
-    var spacing = TWO_PI / segments;
-    var radius = 200;
     //clear the screen
-    context.clearRect(0,0, canvas.width, canvas.height);
+    context.fillStyle = 'rgba(0,0,0,1)';
+    context.fillRect(0, 0, width, height);
+    // context.clearRect(0,0, canvas.width, canvas.height);
 
     //draw circles
-
     context.beginPath();
 
-    for (var i = 0; i < segments; i++) {
-
-        context.strokeStyle = "#FF0000"; //set the line colour to black
-        var x = Math.sin(spacing * i * (mouseX/50)) * Math.cos(spacing * i * (mouseY/50)) * radius;
-        var y = Math.sin(spacing * i* (mouseX/50)) * Math.sin(spacing * i * (mouseY/50)) * radius;
-
-        context.lineTo(x+radius,y+radius);
-    }
-
-    context.stroke(); //draw the outline
+    // for (var i = 0; i < segments; i++) {
+    //
+    //     context.strokeStyle = "#FF0000"; //set the line colour to black
+    //     var x = Math.sin(spacing * i * (mouseX/50)) * Math.cos(spacing * i * (mouseY/50)) * radius;
+    //     var y = Math.sin(spacing * i* (mouseX/50)) * Math.sin(spacing * i * (mouseY/50)) * radius;
+    //
+    //     context.lineTo(x+radius,y+radius);
+    // }
+    //
+    // context.stroke(); //draw the outline
     context.closePath();
     requestAnimationFrame(draw);
 }
