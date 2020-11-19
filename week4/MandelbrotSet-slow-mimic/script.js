df2b16694c0684641238fbbb829424d7a3247edb
Nan Zhao <nanzhao@nyu.edu>
2020-11-19 18:00:24 +0000

    add mouse interaction

--- week4/MandelbrotSet-slow-mimic/script.js
+++ week4/MandelbrotSet-slow-mimic/script.js
@@ -4,8 +4,8 @@ var mouseY = 1;
 var canvas = document.querySelector("canvas");
 canvas.addEventListener('mousemove', getMouse, false);
 
-var width = window.innerWidth;
-var height = window.innerHeight;
+var width = 400;//window.innerWidth;
+var height = 400;//window.innerHeight;
 var context = canvas.getContext("2d");
 canvas.setAttribute("width", width);
 canvas.setAttribute("height", height);
@@ -25,9 +25,16 @@ var imageHeight = height;
 var imageData = context.getImageData(0, 0, imageWidth, imageHeight);
 
 context.translate(width*0.5,height*0.5);//As always with proper stuff, we translate so that 0,0 is in the middle.
+var mouseMapped_min = -2.5;
+var mouseMapped_max = 2.5;
 
-function draw() {
-//var draw = function() {
+//function draw() {
+var draw = function() {
+    mouseMapped_min = map(mouseX, 0, width, -2.5,0);
+    mouseMapped_max = map(mouseY, 0, width, 2.5, 0);
+    minVal = mouseMapped_min;
+    maxVal = mouseMapped_max;
+    // console.log(mouseMapped);
 //We are going to test every single pixel on the screen, so we need nested for loops.
     for (var i = -(width / 2); i < width / 2; i += res) {//for every pixel in the x dimension (columns)...
         for (var j = -(width / 2); j < width / 2; j += res) {//...run through all the y pixels
@@ -48,8 +55,8 @@ function draw() {
                     zy = y + cy;//and then add cy to y to get the next value of zy
                     z = Math.sqrt((zx * zx) + (zy * zy));//Now we get the square of the absolute value of z so that we can see if it's over 2 the next time we run the test. If it is, we don't do anything
 
-                    greyScale = map(test, 0, maxIterations, 0, 255);
-                    context.fillStyle = 'rgb(' + greyScale + ',' + greyScale + ',' + greyScale + ')';
+                    var greyScaleMap = map(test, 0, maxIterations, 0, 1);
+                    greyScale = map(Math.sqrt(greyScaleMap),0,1,0,255 );
 
                     col = 255 - (255 / maxIterations) * test;
                     //context.fillStyle = 'rgb(' + col + ',' + col + ',' + col + ')';//the colour we use depends on how many tests we had to do before the value of z got bigger than 2.
@@ -79,10 +86,11 @@ function draw() {
         }
     }
     context.putImageData(imageData,0,0);
+    requestAnimationFrame(draw);
 }
 
-draw();
-//requestAnimationFrame(draw);
+//draw();
+requestAnimationFrame(draw);
 
 //and that's how you calculate the Mandelbrot set!
 
