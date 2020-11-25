16b090b878afd46ef96cd82e4ec0595497e5db80
Nan Zhao <nanzhao@nyu.edu>
2020-11-25 16:17:37 +0000

    make, a,b , m, n1 as variables

--- week5/SphereGeometry-superShape/scriptSuperShape.js
+++ week5/SphereGeometry-superShape/scriptSuperShape.js
@@ -45,20 +45,43 @@ var size = 200;
 var x=0;
 var y = 0;
 var z = 0;
+var hue = 0;
+
+//variables for super shapes
+var m = 0;
+var a = 1; //a can be changed each frame
+var b = 1;
+var aChange = 0;
+var bChange = 0;
+var nChange = 0;
+var n1 = 0.2;
+var n2 = 1.7;
+var n3 = 1.7;
 
 function draw() {
     var points = [];
+
+    m = map(mouseX, 0, width, 0, 10);
+    //n1 = map(mouseY, 0, height, -1, 1);
+    a = map(Math.sin(aChange),-1,1,0.95,1.05 );
+    b = map(Math.cos(bChange),-1,1,0.95,1.05 );
+    n1 =map(Math.sin(m),-1,1,-1,1 );//map(Math.sin(nChange), -1, 1, -0.5, 0.5);
+    //n2 = 0.05 + Math.sin(10);//map(Math.sin(m), -1, 1, 0.5,1.5);
+   // n3 = n2;
+    aChange += 0.01;
+    bChange += 0.01;
+
     // Now we build the sphere in the loop to do the change
     for (var i =0; i < dim ; i++) {//dim ; i++) {
-
+        //hue = map(i, 0, dim, 0, 360);
         var latitude = map(i, 0, dim, - Math.PI/2, Math.PI/2);//wei du
         //var s = size * Math.sin(spacing /2 * i);//i);// Calculate the size of the current ring
-        var r2 = superShape(latitude,7,0.2,1.7,1.7);
+        var r2 = superShape(latitude,m, n1, n2, n3);
 
         for (var j = 0; j < dim; j++ ) {//dim; j++ ) {// For each ring..
             var longitude = map(j, 0, dim, - Math.PI, Math.PI);//jing du
             // ...create the next point in the circle at the current size s, at the current depth z
-            var r1 = superShape(longitude,7,0.2,1.7,1.7);
+            var r1 = superShape(longitude,m,n1,n2,n3);
             //var point = [Math.cos(spacing * j) * s,Math.sin(spacing * j) * s,z];
             x = size * r1 * Math.cos(longitude) * r2 * Math.cos(latitude);
             y = size * r1 * Math.sin(longitude) * r2 * Math.cos(latitude);
@@ -84,9 +107,7 @@ function draw() {
         z3d = point3d[2];
 
         // z3d -= 1.0;//the speed of the z,moves the points forwards in space
-
         if (z3d < -fov) z3d += 0;
-
         point3d[2] = z3d;
 
         rotateX(point3d,angleX);
@@ -97,31 +118,63 @@ function draw() {
         z3d = point3d[2];
 
         var scale = (fov / (fov + z3d));
-
         var x2d = (x3d * scale) ;//+ HALF_WIDTH / 2;
         var y2d = (y3d * scale) ;//+ HALF_HEIGHT;
 
-// Draw the point
+        if (j===0){//store the first points and use them at the end.
+            firstx2d=x2d;
+            firsty2d=y2d;
+            firstScale=scale;
 
-// Set the size based on scaling
-        context.lineWidth = scale;
+            lastx2d=x2d;
+            lasty2d=y2d;
+            lastScale=scale;
+        }
+
+        // Draw the point
+        // Set the size based on scaling
+        context.lineWidth = scale*2;
         context.save();
         context.translate(HALF_WIDTH,HALF_HEIGHT);
-        context.strokeStyle = "rgb(255,255,255)";
+        // context.strokeStyle = "rgb(255,255,255)";
+
+        //context.strokeStyle = "rgb(255,255,255)";
+        context.strokeStyle =  "hsl("+hue+", 100%, 70%)";
         context.beginPath();
-        context.moveTo(x2d, y2d);
+        //context.moveTo(x2d, y2d);
+        context.moveTo(lastx2d + lastScale, lasty2d);
         context.lineTo(x2d + scale, y2d);
+        //context.closePath();
+        //context.fill();
         context.stroke();
         context.restore();
+
+        // Store the last point so we can join it to the next one.
+        lastx2d=x2d;
+        lasty2d=y2d;
+        lastScale=scale;
+
+        // if it's the end of the current ring, join it to the first
+        if (j===dim-1) {
+            context.lineWidth = scale;
+            //context.strokeStyle = "rgb(255,255,255)";
+            context.beginPath();
+            context.moveTo(lastx2d + lastScale, lasty2d);
+            context.lineTo(firstx2d + firstScale, firsty2d);
+            context.stroke();
+        }
     }
+
+    hue += 0.5;
+    if(hue === 359) hue = 0;
+
     requestAnimationFrame(draw);
 }
 
 requestAnimationFrame(draw);
 
 //http://paulbourke.net/geometry/supershape/
-var a = 1; //a can be changed each frame
-var b = 1;
+//there are global virables a and b
 function superShape(angle, m, n1, n2, n3){
     var t1 = Math.abs((1/a)* Math.cos(m*angle / 4));
     t1 = Math.pow(t1, n2);
