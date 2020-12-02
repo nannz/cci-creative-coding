4f20aafc84597b40a82f83c6b60af64ca231a1ad
Nan Zhao <nanzhao@nyu.edu>
2020-12-02 13:00:52 +0000

    add envMap, add fireFly class

---
+++ week6/threeJS-4-object/Firefly.js
@@ -0,0 +1,33 @@
+import * as THREE from 'https://unpkg.com/three@0.123.0/build/three.module.js';
+
+class Firefly{
+    constructor(r, material, x,y,z){
+        this.radius = r;
+        this.geo = new THREE.IcosahedronBufferGeometry( this.radius,0 );
+        this.material = material;
+        this.mesh = new THREE.Mesh(this.geo, this.material);
+        this.mesh.position.set(x,y,z);//position is a 3Vector
+    }
+
+    update(){
+        this.mesh.rotation.x += 0.005;
+        this.mesh.rotation.y += 0.01;
+    }
+}
+export default Firefly;
+
+
+/*
+* for ( let i = 0; i < 500; i ++ ) {
+
+					const mesh = new THREE.Mesh( geometry, material );
+					mesh.position.x = Math.random() * 10000 - 5000;
+					mesh.position.y = Math.random() * 10000 - 5000;
+					mesh.position.z = Math.random() * 10000 - 5000;
+					mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 3 + 1;
+					scene.add( mesh );
+
+					spheres.push( mesh );
+
+				}
+*/
